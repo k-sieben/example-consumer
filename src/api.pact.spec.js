ffd07dc1348691d37c1699cddcd823f572c94211
Kai Sieben <kai.sieben@bosch.com>
2023-12-11 15:33:30 +0100

    Test

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -17,29 +17,36 @@ describe('API Pact test', () => {
     test('ID 0241e931 exists', async () => {
       // Arrange
 
-      // const expectedProduct = {
-      //   id: '0241e931-9831-413a-9347-6569f0d5fc83',
-      //   type: 'Application',
-      //   name: 'DOORS'
-      // };
-
-      // Uncomment to see this fail
-      // const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees', price: 30.0, newField: 22}
-
-      mockProvider
-        .given('a factsheet with ID 0241e931-9831-413a-9347-6569f0d5fc83 exists')
-        .uponReceiving('a request to get a factsheet')
-        .withRequest({
-          method: 'POST',
-          path: '/graphql',
-          // headers: {
-          //   Authorization: like('Bearer 2019-01-14T11:34:18.045Z')
-          // }
-          headers: {
-            'Content-Type': 'application/json',
+      const expectedResponse =
+      {
+        data: {
+          factSheet: {
+            id: '0241e931-9831-413a-9347-6569f0d5fc83',
+            name: 'DOORS',
+            displayName: 'DOORS',
+            description: 'Description',
+            type: 'Application',
           },
-          body: {
-            query: `
+        },
+      }
+
+        // Uncomment to see this fail
+        // const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees', price: 30.0, newField: 22}
+
+        mockProvider
+        .given('a factsheet with ID 0241e931-9831-413a-9347-6569f0d5fc83 exists')
+          .uponReceiving('a request to get a factsheet')
+          .withRequest({
+            method: 'POST',
+            path: '/graphql',
+            // headers: {
+            //   Authorization: like('Bearer 2019-01-14T11:34:18.045Z')
+            // }
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: {
+              query: `
               query {
                 factSheet(id: "0241e931-9831-413a-9347-6569f0d5fc83") {
                   id
@@ -50,36 +57,37 @@ describe('API Pact test', () => {
                 }
               }
             `,
-          },
-        })
-        .willRespondWith({
-          status: 200,
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          // body: like(expectedResponse)
-          body: {
-            data: {
-              factSheet: {
-                id: '0241e931-9831-413a-9347-6569f0d5fc83',
-                name: 'DOORS',
-                displayName: 'DOORS',
-                description: 'Description',
-                type: 'Application',
-              },
             },
-          },
-        });
-      return mockProvider.executeTest(async (mockserver) => {
-        // Act
-        const api = new API(mockserver.url);
-        const product = await api.getFactSheet('0241e931-9831-413a-9347-6569f0d5fc83');
+          })
+          .willRespondWith({
+            status: 200,
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: like(expectedResponse),
+            // body: {
+            //   data: {
+            //     factSheet: {
+            //       id: '0241e931-9831-413a-9347-6569f0d5fc83',
+            //       name: 'DOORS',
+            //       displayName: 'DOORS',
+            //       description: 'Description',
+            //       type: 'Application',
+            //     },
+            //   },
+            // },
+          });
+        return mockProvider.executeTest(async (mockserver) => {
+          // Act
+          const api = new API(mockserver.url);
+          const factSheet = await api.getFactSheet('0241e931-9831-413a-9347-6569f0d5fc83');
 
-        // Assert - did we get the expected response
-        expect(product).toStrictEqual(new Product(expectedProduct));
-        return;
+          // Assert - did we get the expected response
+          // expect(product).toStrictEqual(new Product(expectedProduct));
+          // return;
+          return new Verifier().verifyInteraction(interaction);
+        });
       });
-    });
 
     // test('product does not exist', async () => {
     //   // set up Pact interactions
